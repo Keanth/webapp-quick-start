f97d49b3e53d4c77c8fd347459e654d142c02e58
Kenneth Lenaerts <Lenaerts.kenneth@gmail.com>
2017-03-31 16:02:39 +0200

    addes hot reloader for css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-'use strict';
+// 'use strict';
 
-// Allow webpack to include ./app.css in bundle
-const css = require( './app.scss' );
+// // Allow webpack to include ./app.css in bundle
+// const css = require( './app.scss' );
 
-console.log( 'Hello from server' );
+// console.log( 'Hello from server' );
+
+import css from './app.scss';
+
+console.log( 'testing hot module' );
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 const path = require( 'path' );
+const webpack = require ( 'webpack' );
+
+var isProd = process.env.NODE_ENV === 'production'; // boolean
+const cssDev = [ 'style-loader', 'css-loader', 'sass-loader' ];
+const cssProd = ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader'],
+					publicPath: '/dist'
+				});
+var cssConfig = isProd ? cssProd : cssDev; 
 
 module.exports = {
 	entry: {
 		app: './src/app.js',
-		contact: './src/other.js'
+		// contact: './src/other.js'
 	},
 	output: {
 		path: path.join( __dirname, 'dist' ),
@@ -14,26 +24,32 @@ module.exports = {
 	module: {
 		rules: [
 			{ 
-				test: /\.scss$/, use: ExtractTextPlugin.extract({
-					fallbackLoader: 'style-loader',
-					loader: ['css-loader', 'sass-loader'],
-					publicPath: '/dist'
-				}) 
+				test: /\.scss$/, 
+				use: cssConfig
 			},
+			// { 
+			// 	test: /\.scss$/, 
+			// 	use: ['style-loader', 'css-loader', 'sass-loader']
+			// },
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
 				use: 'babel-loader' 
 			},
 			{
-				test: /\.pug/,
-				loaders: ['raw-loader', 'pug-html-loader']
+				test: /\.pug$/,
+				loaders: 'pug-loader'
+			},
+			{
+				test: /\.png$/,
+				use: 'file-loader'
 			}
 		]
 	},
 	devServer: {
 		contentBase: path.join( __dirname, 'dist' ),
 		compress: true,
+		hot: true,
 		// IT'S OVER 9000
 		port: 9001,
 		stats: 'errors-only'
@@ -48,17 +64,19 @@ module.exports = {
 			excludeChunks: [ 'contact' ],
 			template: './src/index.pug'
 		}),
-		new HtmlWebpackPlugin({
-			title: 'Other Page',
-			hash: true,
-			chunks: [ 'Other' ],
-			filename: 'Other.html',
-			template: './src/Other.ejs'
-		}),
+		// new HtmlWebpackPlugin({
+		// 	title: 'Other Page',
+		// 	hash: true,
+		// 	chunks: [ 'Other' ],
+		// 	filename: 'Other.html',
+		// 	template: './src/Other.ejs'
+		// }),
 		new ExtractTextPlugin({
 			filename: 'app.css',
-			disable: false,
+			disable: !isProd,
 			allChunks: true
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin()
 	]
 }
