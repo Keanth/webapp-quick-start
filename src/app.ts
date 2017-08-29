464c53042efce628d4bb62240ac7eeb6ee2f5a34
Kenneth Lenaerts <Lenaerts.Kenneth@gmail.com>
2017-08-29 14:51:07 +0200

    Quick clean up

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,5 @@
-import * as $ from 'jquery';
-import userService from './userService';
+// Pull in jQuery, should you need it
+// import * as $ from 'jquery';
 
-// // Allow webpack to include ./app.css in bundle
+// Allow webpack to include ./app.css in bundle
 import './app.sass';
-
-const users = userService.getAll();
-for (const user of users) {
-  $('#list').append(`<li>${user.name}</li>`);
-}
